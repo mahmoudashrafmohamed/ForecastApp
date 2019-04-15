a0050059e5e0dcda01c9673f5e2ad0915c768ac5
mahmoud_ashraf <ashraaf.mahmoud@gmail.com>
2019-04-15 22:01:39 +0200

    add BottomNavigationView

--- app/src/main/java/com/mahmoud_ashraf/forecastapp/ui/MainActivity.kt
+++ app/src/main/java/com/mahmoud_ashraf/forecastapp/ui/MainActivity.kt
@@ -1,13 +1,35 @@
 package com.mahmoud_ashraf.forecastapp.ui
 
+import android.graphics.Typeface
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mahmoud_ashraf.forecastapp.R
+import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.TextView
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        // change font
+        for (i in 0 until toolbar.childCount) {
+            val view = toolbar.getChildAt(i)
+            if (view is TextView) {
+                val titleFont = Typeface.createFromAsset(assets, "fonts/daisy.ttf")
+                if (view.text == toolbar.title) {
+                    view.typeface = titleFont
+                    break
+                }
+            }
+        }
+
+
+
+
     }
 }
